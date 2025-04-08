410be9e9da69ae9966b8247911592eaf13031a36
meemmahmuda <meemmahmuda85@gmail.com>
2025-04-08 16:13:04 +0600

    first commit

--- src/app/layout/default-layout/_nav.ts
+++ src/app/layout/default-layout/_nav.ts
@@ -2,6 +2,13 @@ import { INavData } from '@coreui/angular';
 
 export const navItems: INavData[] = [
   {
+    title: true,
+    name: 'হোল্ডিং কর',
+    attributes: {
+      style: 'color: #fff; font-weight: bold; font-size: 11px; font-family: "Noto Sans Bengali", sans-serif; text-shadow: 1px 1px #000;'
+    }
+  },
+  {
     name: 'Dashboard',
     url: '/dashboard',
     iconComponent: { name: 'cil-speedometer' },
